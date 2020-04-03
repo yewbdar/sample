b813b11457df310f75d13f886cd94f61821af8dd
yewbdar <Yewbdar.Grima@Gmail.com>
2020-04-03 11:32:49 -0500

    componenet functionality added

--- src/component/burger/Burger.js
+++ src/component/burger/Burger.js
@@ -2,14 +2,25 @@ import React from 'react'
 import BurgerIngerident from './burgerIngeridient/BurgerIngerident'
 import './burger.css'
 
-const Burger = (props) =>{
+
+const Burger = (props) => {
+    let transformedBurger = Object.keys(props.ingerident)
+    .map(igkey => {
+        return [...Array(props.ingerident[igkey])]
+        .map((_,i) => {
+            return <BurgerIngerident key={igkey + i} type={igkey}/>
+        })
+    })
+    .reduce((str , cur) => { return str.concat(cur)},[])
+    
+  if(transformedBurger.length === 0) transformedBurger = <p>please start adding ingerident </p>
     return (
         <div className='burger'>
+            
             <BurgerIngerident type='bread-top' />
-            <BurgerIngerident type='cheese' />
-            <BurgerIngerident type='salad' />
-            <BurgerIngerident type='meat' />
+             {transformedBurger}
             <BurgerIngerident type='bread-bottem' />
+           
         </div>
     )
 }
